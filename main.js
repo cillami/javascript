f888fe0d5033cbf569aef64b99fafc24e6c17e0a
cillami <ceciliaminami@Cecilias-MacBook-Pro.local>
2016-11-08 14:04:02 +0100

    javascript ovning

---
+++ main.js
@@ -0,0 +1,33 @@
+/*  */
+h1 {
+	color: green;
+	font-size: 50px;
+}
+/*  punkt sen namnet på klassen för att referera */
+.my-class{
+	color: lightsalmon;
+}
+/*  hashtag sen namnet på id för att referera */
+#my-id{
+	color:teal;
+}
+
+.my-div{
+	width:200px;
+	height:200px;
+	background-color:orange;
+	border: 1px solid #000;
+	display: inline-block;
+	margin: 10px;
+	margin: 10px 10px 20px 10px;
+	/*  short hand som klockan (på margin), 20px top, 20px right, 20 px bottom, 20 px left */
+	margin-bottom: 20px;
+	padding: 10px;
+	margin: 0 auto;
+}
+
+.my-div2{
+	width:200px;
+	height:200px;
+	background-color:blue;
+}
