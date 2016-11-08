b7b4923c18d92f8f3898c238d9ffba339efde1ae
cillami <ceciliaminami@Cecilias-MacBook-Pro.local>
2016-11-08 14:59:05 +0100

    ovning

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
-/*  */
-h1 {
-	color: green;
-	font-size: 50px;
-}
-/*  punkt sen namnet på klassen för att referera */
-.my-class{
-	color: lightsalmon;
-}
-/*  hashtag sen namnet på id för att referera */
-#my-id{
-	color:teal;
-}
+var myDiv = document.createElement("div");
 
-.my-div{
-	width:200px;
-	height:200px;
-	background-color:orange;
-	border: 1px solid #000;
-	display: inline-block;
-	margin: 10px;
-	margin: 10px 10px 20px 10px;
-	/*  short hand som klockan (på margin), 20px top, 20px right, 20 px bottom, 20 px left */
-	margin-bottom: 20px;
-	padding: 10px;
-	margin: 0 auto;
-}
+var myParagraf = document.createElement("p");
 
-.my-div2{
-	width:200px;
-	height:200px;
-	background-color:blue;
-}
+var myTextNode = document.createTextNode("Satan i gatan vad kul Javascript är!");
+
+myParagraf.appendChild(myTextNode);
+
+myDiv.appendChild(myParagraf);
+
+document.body.appendChild(myDiv);
+
+myParagraf.id = "minPtagg"; 
+
+var p = document.getElementById("minPtagg");
+
+minPtagg.innerHTML = "Satan vad tråkigt Javascript är!";
+
+minPtagg.style.color= "blue";
+
+
